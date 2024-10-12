87844d42f886e36bb66805ef0e02a57e7336050b
powerkyungil <92499273+powerkyungil@users.noreply.github.com>
2024-10-12 18:33:30 +0900

    Fix

--- index.php
+++ index.php
@@ -15,21 +15,23 @@ $menu = $_GET['menu']?? "";
 
 // includeHeader($menu);
 
-if (isset($_GET['userId']) && $_GET['userId'] != "") {
-    include "view/item/herenothere/mainPage.php";
-} else {
-    if ($menu == 'PDF') {
-        include "view/item/transPDF.php";
-    } elseif ($menu == 'check') {
-        include "view/item/textCheck.php";
-    } elseif ($menu == 'portfolio') {
-        include "view/item/portfolio.php";
-    } elseif ($menu == 'hnh') {
-        include "view/item/herenothere/main.php";
-    } else {
-        include "view/item/list.php";
-    }
-}
+// if (isset($_GET['userId']) && $_GET['userId'] != "") {
+//     include "view/item/herenothere/mainPage.php";
+// } else {
+//     if ($menu == 'PDF') {
+//         include "view/item/transPDF.php";
+//     } elseif ($menu == 'check') {
+//         include "view/item/textCheck.php";
+//     } elseif ($menu == 'portfolio') {
+//         include "view/item/portfolio.php";
+//     } elseif ($menu == 'hnh') {
+//         include "view/item/herenothere/main.php";
+//     } else {
+//         include "view/item/list.php";
+//     }
+// }
+
+include "view/item/herenothere/main.php";
 
 
 
