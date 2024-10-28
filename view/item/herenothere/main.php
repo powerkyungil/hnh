c3375f01bc7c06b4135ad95e4845e05ef7b65a1b
powerkyungil <92499273+powerkyungil@users.noreply.github.com>
2024-10-28 15:53:42 +0900

    Fix

--- view/item/herenothere/main.php
+++ view/item/herenothere/main.php
@@ -1,5 +1,5 @@
 <?php
-
+session_start();
 ?>
 
 <!DOCTYPE html>
@@ -7,7 +7,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>로그인 페이지</title>
+    <title>HNH</title>
     <link rel="stylesheet" href="/css/default.css">
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <script>
